4f9730dc0d5a68de2a8592db0e1443339343b80c
pathmedaService <72338499+pathmedaService@users.noreply.github.com>
2020-10-04 21:29:21 +0530

    git commit

---
+++ build/precache-manifest.5277c81c3a00a683ab15859be1a32853.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d50d312dd819347ab330281d581560",
+    "url": "/pathmedaliveweb/index.html"
+  },
+  {
+    "revision": "a38cd8ac4d1af53b3f93",
+    "url": "/pathmedaliveweb/static/css/main.c41a78cb.chunk.css"
+  },
+  {
+    "revision": "56c533ac78f97f12a7a8",
+    "url": "/pathmedaliveweb/static/js/2.8cace665.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pathmedaliveweb/static/js/2.8cace665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a38cd8ac4d1af53b3f93",
+    "url": "/pathmedaliveweb/static/js/main.1c123208.chunk.js"
+  },
+  {
+    "revision": "5d0593b504c0e4c645e6",
+    "url": "/pathmedaliveweb/static/js/runtime-main.8ced7d4c.js"
+  },
+  {
+    "revision": "25bad26557f3c242fbd1965b28e70571",
+    "url": "/pathmedaliveweb/static/media/IndiaFarming.25bad265.jpg"
+  },
+  {
+    "revision": "75bff033e3f0b032783c6974f5d1fb42",
+    "url": "/pathmedaliveweb/static/media/call-to-action-bg.75bff033.jpg"
+  },
+  {
+    "revision": "261542bd6c4b00c849eeb6476a193683",
+    "url": "/pathmedaliveweb/static/media/cows.261542bd.jpg"
+  }
+]);
